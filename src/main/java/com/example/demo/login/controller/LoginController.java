a4dbde08d13653137da9c7dc34d7c88cf66dfd17
金田尚大 <1111510078r21@gmail.com>
2020-12-03 15:45:06 +0900

    バリデーション追加

--- src/main/java/com/example/demo/login/controller/LoginController.java
+++ src/main/java/com/example/demo/login/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.example.demo.login.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ public class LoginController {
 	}
 
 	@PostMapping("/login")
-	public String postLogin(@ModelAttribute LoginForm form, BindingResult bindingResult, Model model) {
+	public String postLogin(@ModelAttribute @Validated LoginForm form, BindingResult bindingResult, Model model) {
 		if (bindingResult.hasErrors()) {
 			return getLogin(form, model);
 		}
--- src/main/java/com/example/demo/login/domain/model/LoginForm.java
+++ src/main/java/com/example/demo/login/domain/model/LoginForm.java
@@ -1,16 +1,16 @@
 package com.example.demo.login.domain.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class LoginForm {
-	/*
-	 * user ID
-	 */
+	@NotBlank
+	@Email
 	private String userId;
 
-	/**
-	 * password
-	 */
+	@NotBlank
 	private String password;
 }
