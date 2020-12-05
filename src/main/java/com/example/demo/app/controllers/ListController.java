628d06d20f2e2b9df40aed0df0a93ad572101504
金田尚大 <1111510078r21@gmail.com>
2020-12-05 12:51:23 +0900

    コントローラーからuser serviceを使用する

--- src/main/java/com/example/demo/app/controllers/ListController.java
+++ src/main/java/com/example/demo/app/controllers/ListController.java
@@ -1,11 +1,17 @@
 package com.example.demo.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.domain.services.UserService;
+
 @Controller
 public class ListController {
+	@Autowired
+	private UserService userService;
+
 	@GetMapping("/list")
 	public String index(Model model) {
 		return "list/index";
