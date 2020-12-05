5dd96695dd856eeb70a4b2c8b80cce82c951d407
金田尚大 <1111510078r21@gmail.com>
2020-12-05 12:50:55 +0900

    user service

---
+++ src/main/java/com/example/demo/domain/services/UserService.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+//	public User getAll() throws InterruptedException, ExecutionException {
+//		// TODO: get users
+//	}
+}
